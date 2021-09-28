7e6277040ef56c5ae8adb4cb22cd679c731da84a
baiama <primkulovbaiaman@gmail.com>
2021-09-29 01:42:21 +0600

    g

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'details.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,23 +14,6 @@ class Home extends StatelessWidget {
             Navigator.push(context,
                 MaterialPageRoute(builder: (context) => const MyDetailsPage()));
           },
-          child: const Text('Hello'),
-        ),
-      ),
-    );
-  }
-}
-
-class MyDetailsPage extends StatelessWidget {
-  const MyDetailsPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
           child: const Text('Hello'),
         ),
       ),
