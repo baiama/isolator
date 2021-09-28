79dfe7482127102793baa6dea0ba90eab3791e55
baiama <primkulovbaiaman@gmail.com>
2021-09-29 02:19:49 +0600

    gg

--- lib/service/worker.dart
+++ lib/service/worker.dart
@@ -13,8 +13,7 @@ class MyWorker {
   void _init() async {
     _receivePort = ReceivePort();
     _receivePort.listen(_handleMessage);
-    _isolate = await Isolate.spawn(
-        _isolateEntry, _receivePort.sendPort.send("message"));
+    _isolate = await Isolate.spawn(_isolateEntry, _receivePort.sendPort);
   }
 
   void dispose() {
@@ -27,6 +26,10 @@ class MyWorker {
     print('state delay');
     await Future.delayed(const Duration(seconds: 2));
     print('end delay');
+    if (message is SendPort) {
+      message.send("work end");
+    }
+    message.send("work end");
   }
 
   void _handleMessage(message) {
