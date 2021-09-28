df069ed69426fbebf9a9a051fe4dad105f8daa34
baiama <primkulovbaiaman@gmail.com>
2021-09-29 02:22:43 +0600

    gg

--- lib/service/worker.dart
+++ lib/service/worker.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 import 'dart:isolate';
 
-import 'package:flutter/material.dart';
-
 class MyWorker {
   late Isolate _isolate;
   late ReceivePort _receivePort;
@@ -27,12 +25,15 @@ class MyWorker {
     await Future.delayed(const Duration(seconds: 2));
     print('end delay');
     if (message is SendPort) {
-      message.send("work end");
+      message.send("yo22");
     }
     message.send("work end");
   }
 
   void _handleMessage(message) {
     print("handle message $message");
+    if (message is String && message == 'yo22') {
+      dispose();
+    }
   }
 }
